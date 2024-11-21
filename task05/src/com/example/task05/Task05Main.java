93f808c420617f9783a17e2018610ecfc524564a
Alexey Postnov <adpostnov@yandex.ru>
2024-11-21 23:18:39 +0500

    Постнов Алексей. Задание 05

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -6,12 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Task05Main {
-
     public static void main(String[] args) {
-
-        /*
-
-        // Random variables
         String randomFrom = "..."; // Некоторая случайная строка. Можете выбрать ее самостоятельно.
         String randomTo = "...";  // Некоторая случайная строка. Можете выбрать ее самостоятельно.
         int randomSalary = 100;  // Некоторое случайное целое положительное число. Можете выбрать его самостоятельно.
@@ -20,8 +15,7 @@ public class Task05Main {
         MailMessage firstMessage = new MailMessage(
                 "Robert Howard",
                 "H.P. Lovecraft",
-                "This \"The Shadow over Innsmouth\" story is real masterpiece, Howard!"
-        );
+                "This \"The Shadow over Innsmouth\" story is real masterpiece, Howard!");
 
         assert firstMessage.getFrom().equals("Robert Howard") : "Wrong firstMessage from address";
         assert firstMessage.getTo().equals("H.P. Lovecraft") : "Wrong firstMessage to address";
@@ -30,18 +24,14 @@ public class Task05Main {
         MailMessage secondMessage = new MailMessage(
                 "Jonathan Nolan",
                 "Christopher Nolan",
-                "Брат, почему все так хвалят только тебя, когда практически все сценарии написал я. Так не честно!"
-        );
+                "Брат, почему все так хвалят только тебя, когда практически все сценарии написал я. Так не честно!");
 
         MailMessage thirdMessage = new MailMessage(
                 "Stephen Hawking",
                 "Christopher Nolan",
-                "Я так и не понял Интерстеллар."
-        );
+                "Я так и не понял Интерстеллар.");
 
-        List<MailMessage> messages = Arrays.asList(
-                firstMessage, secondMessage, thirdMessage
-        );
+        List<MailMessage> messages = Arrays.asList(firstMessage, secondMessage, thirdMessage);
 
         // Создание почтового сервиса.
         MailService<String> mailService = new MailService<>();
@@ -53,25 +43,21 @@ public class Task05Main {
         //   где по получателю можно получить список сообщений, которые были ему отправлены
         Map<String, List<String>> mailBox = mailService.getMailBox();
 
-        assert mailBox.get("H.P. Lovecraft").equals(
-                Arrays.asList(
-                        "This \"The Shadow over Innsmouth\" story is real masterpiece, Howard!"
-                )
-        ) : "wrong mailService mailbox content (1)";
+        assert mailBox.get("H.P. Lovecraft").equals(Arrays.asList("This \"The Shadow over Innsmouth\" story is real masterpiece, Howard!")) : "wrong mailService mailbox content (1)";
 
-        assert mailBox.get("Christopher Nolan").equals(
-                Arrays.asList(
-                        "Брат, почему все так хвалят только тебя, когда практически все сценарии написал я. Так не честно!",
-                        "Я так и не понял Интерстеллар."
-                )
-        ) : "wrong mailService mailbox content (2)";
+        assert mailBox.get("Christopher Nolan").equals(Arrays.asList("Брат, почему все так хвалят только тебя, когда практически все сценарии написал я. Так не честно!", "Я так и не понял Интерстеллар.")) : "wrong mailService mailbox content (2)";
 
         assert mailBox.get(randomTo).equals(Collections.<String>emptyList()) : "wrong mailService mailbox content (3)";
 
-
         // Создание списка из трех зарплат.
-        Salary salary1 = new Salary("Facebook", "Mark Zuckerberg", 1);
-        Salary salary2 = new Salary("FC Barcelona", "Lionel Messi", Integer.MAX_VALUE);
+        Salary salary1 = new Salary(
+                "Facebook",
+                "Mark Zuckerberg",
+                1);
+        Salary salary2 = new Salary(
+                "FC Barcelona",
+                "Lionel Messi",
+                Integer.MAX_VALUE);
         Salary salary3 = new Salary(randomFrom, randomTo, randomSalary);
 
         // Создание почтового сервиса, обрабатывающего зарплаты.
@@ -86,10 +72,5 @@ public class Task05Main {
         assert salaries.get(salary1.getTo()).equals(Arrays.asList(1)) : "wrong salaries mailbox content (1)";
         assert salaries.get(salary2.getTo()).equals(Arrays.asList(Integer.MAX_VALUE)) : "wrong salaries mailbox content (2)";
         assert salaries.get(randomTo).equals(Arrays.asList(randomSalary)) : "wrong salaries mailbox content (3)";
-
-
-        */
-
     }
-
 }
